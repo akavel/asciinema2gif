0fcb80d86c7b51e8a4e16fcabc692089671689d1
Mateusz Czapliński <czapkofan@gmail.com>
2018-10-22 02:26:12 +0200

    create simulated terminal window image

--- asciinema2gif.go
+++ asciinema2gif.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"os"
 
 	"github.com/cirocosta/asciinema-edit/cast"
 	"github.com/golang/freetype/truetype"
 	ansi "github.com/icecrime/ansi/internals"
 	fontdata "golang.org/x/image/font/gofont/gomono"
+	"golang.org/x/image/math/fixed"
 )
 
 func main() {
@@ -15,13 +18,14 @@ func main() {
 	if err != nil {
 		die(err.Error())
 	}
-	_ = c
 
 	font, err := truetype.Parse(fontdata.TTF)
 	if err != nil {
 		die(err.Error())
 	}
-	_ = font
+
+	scr := NewScreen(int(c.Header.Width), int(c.Header.Height), font)
+	_ = scr
 
 	for _, ev := range c.EventStream {
 		if ev.Type != "o" {
@@ -33,9 +37,32 @@ func main() {
 		}
 	}
 
+	// TODO: loop events:
+	// TODO: - parse event's data
+	// TODO: - render event's contents on simulated window, using the font
+	// TODO: - add image into gif struct
+	// TODO: render animated gif asciinema.gif
 }
 
 func die(msg string) {
 	os.Stdout.WriteString("error: " + msg + "\n")
 	os.Exit(1)
+}
+
+func NewScreen(w, h int, font *truetype.Font) Screen {
+	// FIXME: variable scale, as flag
+	b := font.Bounds(fixed.I(10))
+	cw := b.Max.X - b.Min.X
+	ch := b.Max.Y - b.Min.Y
+	rect := image.Rect(0, 0, cw.Ceil(), ch.Ceil())
+	palette := make(color.Palette, 256)
+	return Screen{
+		Image:      image.NewPaletted(rect, palette),
+		CellBounds: b,
+	}
+}
+
+type Screen struct {
+	Image      *image.Paletted
+	CellBounds fixed.Rectangle26_6
 }
